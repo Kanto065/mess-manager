73c7260b7331db5111de815125539cb1122ed85c
Kanta Bhattacharjee <kanto.uv@outlook.com>
2023-06-12 04:11:05 +0600

    update meal created

---
+++ src/Components/Manager/Meal Update/MealUpdate.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MealUpdate = () => {
+    return (
+        <div>
+            <h2>Update Meal</h2>
+            <p>date</p>
+
+        </div>
+    );
+};
+
+export default MealUpdate;
